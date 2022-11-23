b9d5d94a8c28eee11d3cac70449dd5df469a39b0
annt <annt@gft.com>
2022-11-23 16:51:13 +0000

    Done with simple logic (100% faster)

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,48 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+
+    public Solution(){
+
+    }
+
+    public void sortColors(int[] nums){
+
+        int red = 0, white = 0, blue = 0, count = 0;
+
+        for (int num : nums) {
+
+            if (num == 0) {
+
+                red++;
+            } else if (num == 1) {
+
+                white++;
+            } else {
+
+                blue++;
+            }
+        }
+
+            while(red != 0){
+
+                nums[count] = 0;
+                count ++;
+                red --;
+            }
+
+            while(white != 0){
+
+                nums[count] = 1;
+                count ++;
+                white --;
+            }
+
+            while(blue != 0){
+
+                nums[count] = 2;
+                count ++;
+                blue --;
+            }
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,47 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+    int[] nums;
+    int[] result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyTwoColors(){
+
+        nums = new int[] {0,1,1,0,1,1,0};
+
+        result = new int[] {0,0,0,1,1,1,1};
+
+        solution.sortColors(nums);
+
+        assertEquals(Arrays.toString(result), Arrays.toString(nums));
+
+    }
+
+    @Test
+    public void ThreeColors(){
+
+        nums = new int[] {2,1,1,0,2,1,0,2,1,2,0,2};
+
+        result = new int[] {0,0,0,1,1,1,1,2,2,2,2,2};
+
+        solution.sortColors(nums);
+
+        assertEquals(Arrays.toString(result), Arrays.toString(nums));
+
+    }
 }
